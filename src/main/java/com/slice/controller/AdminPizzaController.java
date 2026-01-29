9924999593d42f12844184dd7b03e5aef7961577
Athma Joseph <37631703+AthmaJoseph@users.noreply.github.com>
2026-01-29 09:56:38 +0530

    home page and registration page added

--- src/main/java/com/slice/controller/AdminPizzaController.java
+++ src/main/java/com/slice/controller/AdminPizzaController.java
@@ -1,5 +1,10 @@
 package com.slice.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AdminPizzaController {
+	
+	
 
 }
--- src/main/java/com/slice/controller/AuthController.java
+++ src/main/java/com/slice/controller/AuthController.java
@@ -1,5 +1,25 @@
 package com.slice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.slice.model.User;
+
+@Controller
 public class AuthController {
 
+//	@Autowired
+//	private UserService userservice;
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	@GetMapping("/register")
+	public String showRegister(Model model) {
+	model.addAttribute("user",new User());
+	return "register";
+	}
 }
