e531f4a1cfc5a90948d8b0f669cf87935eb5492d
Athma Joseph <37631703+AthmaJoseph@users.noreply.github.com>
2026-01-30 11:14:49 +0530

    separate header and footer

--- src/main/java/com/slice/config/SecurityConfig.java
+++ src/main/java/com/slice/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.slice.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecurityConfig {
 
+   
 }
--- src/main/java/com/slice/controller/AuthController.java
+++ src/main/java/com/slice/controller/AuthController.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.slice.model.User;
+import com.slice.service.UserService;
 
 @Controller
 public class AuthController {
 
-//	@Autowired
-//	private UserService userservice;
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/")
 	public String home() {
@@ -21,5 +25,22 @@ public class AuthController {
 	public String showRegister(Model model) {
 	model.addAttribute("user",new User());
 	return "register";
+	}
+	@PostMapping("/register")
+	public String registerUser(@ModelAttribute("user") User user,Model model) {
+	userService.registerCustomer(user);
+	model.addAttribute("message","Verification link sent to your email ");
+	return "login";
+	}
+	@GetMapping("/verify")
+	public String verifyUser(@RequestParam("token") String token,Model model) {
+		try {
+			userService.verifyUser(token);
+			model.addAttribute("message","Account Verified successfully. You can login now.");
+			}
+		catch(Exception e) {
+			model.addAttribute("error",e.getMessage());
+		}
+		return "login";
 	}
 }
